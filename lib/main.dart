14791706ca79502741b0058dbf899553337edbd5
Not <markangelo.florencio-25@cpu.edu.ph>
2026-08-31 21:04:31 +0800

    done: first step

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MiniStore());
+
+class MiniStore extends StatelessWidget {
+  const MiniStore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mini Store',
+      home: const Scaffold(
+        body: Center(
+          child: Text('Welcome to Mini Store.'),
+        ),
+      ),
+    );
+  }
+}
